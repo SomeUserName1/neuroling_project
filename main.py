b64c986ead92529861c6f5c844743f9a8149201c
Fabian Klopfer <fabian.klopfer@uni-konstanz.de>
2019-03-24 20:57:47 +0100

    upd

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import scipy.io as sio
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix, classification_report
+
+from models import EEGLangComprehension
 import argparse
 import os
 import numpy as np
@@ -8,29 +12,70 @@ import time
 def parse_mat_files(path, export_to_csv):
     coherence_spectra = []
     comprehension_scores = []
+    frequencies = []
+
     for mat_file in sorted(os.listdir(path)):
         mat_contents = sio.loadmat(os.sep.join([path, mat_file]))
         for i in range(0, 2):
+            electrodes = []
+            cluster_indexes = []
             session_i = mat_contents['conn'][0, i][0]
-            coherence_spectra.append(session_i['cohspctrm'][0])
-            for j in range(0,len(session_i['trialinfo'][0])):
+
+            label_struct = session_i['labelcmb'][0]
+            for label in label_struct:
+                electrodes = np.append(electrodes, label[0])
+
+            for el in ['AFZ', 'C2', 'C4', 'CP4', 'CP6', 'F1']:
+                cluster_indexes.append(np.where(electrodes == el)[0][0])
+
+            coherence_spectra.append(session_i['cohspctrm'][0][:, cluster_indexes, :])
+
+            for j in range(0, len(session_i['trialinfo'][0])):
                 comprehension_scores.append(session_i['trialinfo'][0][j][2])
+
+            if len(frequencies) == 0:
+                frequencies = session_i['freq']
+                frequencies = np.concatenate([x for x in frequencies]).flatten()
 
     coherence_spectra = np.concatenate([x for x in coherence_spectra])
     comprehension_scores = np.array(comprehension_scores)
+
     if export_to_csv:
-        coherence_spectra.tofile(path + '\..\coherence_spectra.csv', sep=',')
-        comprehension_scores.tofile(path + '\..\comprehension_scores.csv', sep=',')
-    return coherence_spectra, comprehension_scores
+        coherence_spectra.tofile(path + '\\..\\coherence_spectra.csv', sep=',')
+        comprehension_scores.tofile(path + '\\..\\comprehension_scores.csv', sep=',')
+        frequencies.tofile(path + '\\..\\frequencies.csv', sep=',')
+
+    return coherence_spectra, comprehension_scores, frequencies
 
 
 def main(data_path):
     start = time.time()
-    spectra, scores = parse_mat_files(data_path, True)
+    spectra, scores, frequencies = parse_mat_files(data_path, False)
     end = time.time()
     print(end - start)
-    print(spectra.shape)
-    print(scores.shape)
+
+    for i in [5, 10]:
+        classifier_i = EEGLangComprehension.EEGLangComprehension(frequencies, i)
+        start = time.time()
+        preprocessed_x = classifier_i.preprocess(spectra)
+        end = time.time()
+        print(end - start)
+
+        print(preprocessed_x.shape[0])
+        print(scores.shape[0])
+        start = time.time()
+        classifier_i.fit(preprocessed_x, scores, time_limit=1 * 60 * 60)
+        end = time.time()
+        print(end - start)
+        #kf = KFold(n_splits=2)
+        #for train_index, test_index in kf.split(preprocessed_x):
+        #    train_x, test_x = preprocessed_x[train_index], preprocessed_x[test_index]
+        #    train_y, test_y = preprocessed_y[train_index], preprocessed_y[test_index]
+        #    classifier_i.final_fit(train_x, train_y, test_x, test_y)
+        #    classifier_i.evaluate(test_x, test_y)
+        #    results = classifier_i.predict(test_x)
+        #    print(confusion_matrix(test_y, results))
+        #    print(classification_report(test_y, results))
 
 
 if __name__ == "__main__":
@@ -38,7 +83,8 @@ if __name__ == "__main__":
 
     # TODO final interface
     parser.add_argument("--data_path",
-                        default='C:\\Users\\Fabi\\ownCloud\\workspace\\uni\\7\\neuroling\\neuroling_project\\data\\v1', type=str)
+                        default='C:\\Users\\Fabi\\ownCloud\\workspace\\uni\\7\\neuroling\\neuroling_project\\data\\v1',
+                        type=str)
 
     args = parser.parse_args()
 
--- models/EEGLangComprehension.py
+++ models/EEGLangComprehension.py
@@ -1,11 +1,30 @@
-from autokeras.supervised import DeepTaskSupervised
-from autokeras.constant import Constant
+from bisect import bisect_left, bisect_right
+
+from autokeras.supervised import DeepTaskSupervised, PortableDeepSupervised
 from autokeras.nn.loss_function import classification_loss
 from autokeras.nn.metric import Accuracy, MSE
 from autokeras.preprocessor import OneHotEncoder, DataTransformer
+from autokeras.utils import pickle_to_file
+import numpy as np
 
 
 class EEGLangComprehension(DeepTaskSupervised):
+
+    def __init__(self, frequencies, frequency_component, **kwargs):
+        """
+
+        :param electrodes: an array that defines the electrode names and their respective index
+        :param frequencies: the available frequency components to map from Hz to the available frequency components
+        :param electrodes_cluster: the electrodes that shall be used as input data to the classifier.
+                If None all electrodes are used
+        :param frequency_component: the frequencies that shall be used as input data to the classifier.
+                If None all frequencies are used
+        :param kwargs: args of the superclass
+        """
+        self.frequencies = frequencies
+        self.frequency_component = frequency_component
+        super().__init__(**kwargs)
+
     @property
     def metric(self):
         return Accuracy
@@ -21,13 +40,47 @@ class EEGLangComprehension(DeepTaskSupervised):
         pass
 
     def preprocess(self, x):
-        pass
+        preprocessed_x = []
+        if self.frequency_component is not None:
+            if self.frequency_component < 1 or self.frequency_component > 50:
+                raise Exception("Select a frequency between 1 and 50 Hz!")
+            else:
+                preprocessed_x = np.squeeze(x[:, :, self.frequency_lookup()])
+
+        return preprocessed_x
+
+    def frequency_lookup(self):
+        lower = self.frequency_component - 1
+        upper = self.frequency_component + 1
+
+        idx_lower = bisect_right(self.frequencies, lower)
+        idx_upper = bisect_left(self.frequencies, upper)
+
+        return [range(idx_lower, idx_upper + 1)]
+
+    def transform_y(self, y_train):
+        if self.y_encoder is None:
+            self.y_encoder = OneHotEncoder()
+            self.y_encoder.fit(y_train)
+        y_train = self.y_encoder.transform(y_train)
+        return y_train
+
+    def export_autokeras_model(self, model_file_name):
+        """ Creates and Exports the AutoKeras model to the given filename. """
+        portable_model = PortableDeepSupervised(graph=self.cnn.best_model,
+                                                y_encoder=self.y_encoder,
+                                                data_transformer=self.data_transformer,
+                                                path=self.path)
+        pickle_to_file(portable_model, model_file_name)
 
 
 class V1Transformer(DataTransformer):
+    """
+    Do Standardisation/z scoring for the x values
+    """
+
     def transform_train(self, data, targets=None, batch_size=None):
         pass
 
     def transform_test(self, data, targets=None, batch_size=None):
         pass
-
