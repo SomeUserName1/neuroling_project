50f5ee21da1162c152b6878c7e62bc47be695b5c
Fabian Klopfer <fabian.klopfer@uni-konstanz.de>
2019-03-24 01:15:11 +0100

    upd

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import scipy.io as sio
 import argparse
 import os
+import numpy as np
+import time
+
 
 def parse_mat_files(path, export_to_csv):
     coherence_spectra = []
     comprehension_scores = []
     for mat_file in sorted(os.listdir(path)):
-        mat_contents = sio.loadmat(mat_file)
-        for i in range(1,3):
-            session_i = mat_contents['conn'][0, 0][i]
-            coherence_spectra.concatenate = session_i['cohspectrm']
-            comprehension_scores.concatenate = session_i['trialinfo'][5]
-        return coherence_spectra, comprehension_scores
+        mat_contents = sio.loadmat(os.sep.join([path, mat_file]))
+        for i in range(0, 2):
+            session_i = mat_contents['conn'][0, i][0]
+            coherence_spectra.append(session_i['cohspctrm'][0])
+            for j in range(0,len(session_i['trialinfo'][0])):
+                comprehension_scores.append(session_i['trialinfo'][0][j][2])
+
+    coherence_spectra = np.concatenate([x for x in coherence_spectra])
+    comprehension_scores = np.array(comprehension_scores)
+    if export_to_csv:
+        coherence_spectra.tofile(path + '\..\coherence_spectra.csv', sep=',')
+        comprehension_scores.tofile(path + '\..\comprehension_scores.csv', sep=',')
+    return coherence_spectra, comprehension_scores
 
 
-def main(shape_predictor_path):
-    parse_mat_files('')
+def main(data_path):
+    start = time.time()
+    spectra, scores = parse_mat_files(data_path, True)
+    end = time.time()
+    print(end - start)
+    print(spectra.shape)
+    print(scores.shape)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # TODO final interface
-    parser.add_argument("--shape_predictor_path", default=0, type=str)
+    parser.add_argument("--data_path",
+                        default='C:\\Users\\Fabi\\ownCloud\\workspace\\uni\\7\\neuroling\\neuroling_project\\data\\v1', type=str)
 
     args = parser.parse_args()
 
-    if not os.path.exists(args.data_set_dir):
+    if not os.path.exists(args.data_path):
         raise Exception("Data set path given does not exists")
 
-    main(args.shape_predictor_path)
+    main(args.data_path)
--- models/EEGLangComprehension.py
+++ models/EEGLangComprehension.py
@@ -1,1 +1,33 @@
-from autokeras import DeepTaskSupervised
+from autokeras.supervised import DeepTaskSupervised
+from autokeras.constant import Constant
+from autokeras.nn.loss_function import classification_loss
+from autokeras.nn.metric import Accuracy, MSE
+from autokeras.preprocessor import OneHotEncoder, DataTransformer
+
+
+class EEGLangComprehension(DeepTaskSupervised):
+    @property
+    def metric(self):
+        return Accuracy
+
+    @property
+    def loss(self):
+        return classification_loss
+
+    def get_n_output_node(self):
+        return 6
+
+    def init_transformer(self, x):
+        pass
+
+    def preprocess(self, x):
+        pass
+
+
+class V1Transformer(DataTransformer):
+    def transform_train(self, data, targets=None, batch_size=None):
+        pass
+
+    def transform_test(self, data, targets=None, batch_size=None):
+        pass
+
